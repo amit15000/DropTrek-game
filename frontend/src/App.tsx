8887b15c95364b1d620ac8e00191992fd9b3d1bf
Amit Kumar <15000amitkumar@gmail.com>
2024-07-13 17:37:31 +0530

    Frontend Main complated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./assets/Navbar";
 import Home from "./pages/Home";
+import { Game } from "./pages/Game";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/simulation"></Route>
-        <Route path="/game"></Route>
+        <Route path="/game" element={<Game />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Game.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react";
+import { BallManager } from "../game/classes/BallManager";
+import axios from "axios";
+
+export function Game() {
+  const baseURL = "http://localhost:3000";
+  const [ballManager, setBallManager] = useState<BallManager>();
+  const canvasRef = useRef<any>();
+
+  useEffect(() => {
+    const ballManager = new BallManager(canvasRef.current as HTMLCanvasElement);
+    setBallManager(ballManager);
+  }, [canvasRef]);
+
+  const clickHandler = async () => {
+    const response = await axios.post(`${baseURL}/game`, {
+      data: 1,
+    });
+    if (ballManager) {
+      ballManager.addBall(response.data.point);
+    }
+  };
+
+  return (
+    <div className="flex flex-col lg:flex-row items-center justify-center">
+      <canvas ref={canvasRef}></canvas>
+      <button
+        onClick={clickHandler}
+        className="px-2 py-2 text-2xl bg-green-500 text-white font-bold rounded"
+      >
+        Add Ball
+      </button>
+    </div>
+  );
+}
